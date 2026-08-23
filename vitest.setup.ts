cec7862d1fdc8d53826437ecd19fa860216bea4e
jebasinghjoshua24 <jebasinghjoshua.j.cyber29@princedrkvasudevan.com>
2026-08-23 23:03:07 +0530

    test: mock canvas getContext in jsdom to silence gauge warnings

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,1 +1,27 @@
 import '@testing-library/jest-dom/vitest'
+
+// jsdom does not implement HTMLCanvasElement.getContext; mock it so the
+// AnalogGauge animation loop is a safe no-op in component tests.
+const ctxStub = {
+  setTransform: () => {},
+  clearRect: () => {},
+  beginPath: () => {},
+  arc: () => {},
+  moveTo: () => {},
+  lineTo: () => {},
+  stroke: () => {},
+  fill: () => {},
+  fillText: () => {},
+  set fillStyle(_v: string) {},
+  set strokeStyle(_v: string) {},
+  set lineWidth(_v: number) {},
+  set shadowColor(_v: string) {},
+  set shadowBlur(_v: number) {},
+  set textAlign(_v: string) {},
+  set textBaseline(_v: string) {},
+}
+
+Object.defineProperty(HTMLCanvasElement.prototype, 'getContext', {
+  value: () => ctxStub,
+  writable: true,
+})
